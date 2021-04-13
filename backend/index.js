51af3ad580273bc31a2d83b39f18b2368114ce6b
Jack Nash <jacknash42@gmail.com>
2021-04-13 14:15:29 +1000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-
 app.use('/api', require('./router'));
-
 if (process.env.running == 'production') {
 	app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 	});
+} else {
+	require('dotenv').config();
 }
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
